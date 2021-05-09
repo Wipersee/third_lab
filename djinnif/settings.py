bba5532457ef86a2f0a6f1210c0ef6eee64b5d3a
Wipersee <kesterera@gmail.com>
2021-05-09 15:14:26 +0300

    fix

--- djinnif/settings.py
+++ djinnif/settings.py
@@ -141,10 +141,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
-]
-
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
